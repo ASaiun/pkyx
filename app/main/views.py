932f681f28f73d7c0379e7761e1191b338d6b59b
tonnie <tonnie.lwt@gmail.com>
2015-10-16 11:46:16 +0800

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from . import main
 @main.route('/')
 def index():
     pk_form = PkForm()
-    return render_template('index.html',username="Dave",form=pk_form)
+    return render_template('index.html',form=pk_form)
 
 @main.route('/pk', methods=['GET', 'POST'])
 def pk():
@@ -15,5 +15,5 @@ def pk():
         pk1_name = request.form.get('pk1')
         pk2_name = request.form.get('pk2')
         return render_template('pk.html',pk1=pk1_name, pk2=pk2_name)
-    return Response("none")
+    return Response("invalid input")
 
--- test_mongo.py
+++
@@ -1,3 +0,0 @@
-import pymongo
-conn = pymongo.Connection(host='127.0.0.1', port=27017)
-print(conn)
